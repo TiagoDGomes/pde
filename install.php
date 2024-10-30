a446709b43b938d81586aa512b36dc089279eeda
Tiago <tiago.gomes@ifsp.edu.br>
2024-10-30 15:19:40 -0300

    fix install.php

--- install.php
+++ install.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'config.php';
-require_once 'include/Database.php';
+require_once 'classes/Database.php';
 
 Database::startInstance($CONFIG_PDO_CONN,$CONFIG_PDO_USER, $CONFIG_PDO_PASS);
 
