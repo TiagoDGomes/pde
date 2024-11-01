0f45179325060ca37037eb4c00747c92bd7a740a
Tiago Gomes <dev@nerdbr.top>
2024-11-01 16:41:35 -0300

    ordering table

--- include/contents.php
+++ include/contents.php
@@ -45,9 +45,9 @@
             <thead>
                 <tr>
                     <th><input type="checkbox" name="chk_all"></th>
-                    <th>Nome do item</th>
+                    <!-- <th>Patrimônio</th> -->
+                    <th colspan="2">Nome do item</th>
                     <th>Código</th>
-                    <th>Patrimônio</th>
                     <th>Quantidade emprestada</th>
                     <th>Quantidade devolvida</th>
                     <th>Detalhes</th>
@@ -67,61 +67,61 @@
                                 <label for="loan_date_<?= str_replace("/","_", $this_date) ?>" class="date"><?= $this_date ?></label> 
                             </th>
                         </tr>
-                    
-                    <?php endif; ?> 
-                    <?php $last_date = $this_date; ?>
+                        <?php $last_date = $this_date; ?>
+
+                    <?php endif; ?>                     
 
                     <tr class="<?= $item['count_remaining'] == $item['original_count'] ? 'complete' : 'remaining' ?>">
                     
-                    <td>
-                        <input type="checkbox" id="loan_<?= $item['loan_id'] ?>">
-                    </td>
+                        <td>
+                            <input type="checkbox" id="loan_<?= $item['loan_id'] ?>">
+                        </td>
+                        <td class="number">
+                            <?php if ($item['patrimony_number']): ?>
+                                <span class="patrimony">
+                                    <i class="icon pat"></i> 
+                                    <?= $item['patrimony_number'] ?>
+                                </span>
+                            <?php endif; ?>
+                        </td>
+                        <td>
+                            <label for="loan_<?= $item['loan_id'] ?>"><?= $item['model_name'] ?></label>
+                        </td>
 
-                    <td>
-                        <label for="loan_<?= $item['loan_id'] ?>"><?= $item['model_name'] ?></label>
-                    </td>
+                        <td class="number"><?= $item['model_code'] ?></td>
 
-                    <td class="number"><?= $item['model_code'] ?></td>
-
-                    <td class="number">
-                        <?php if ($item['patrimony_number']): ?>
-                            <span class="patrimony">
-                                <i class="icon pat"></i> 
-                                <?= $item['patrimony_number'] ?>
-                            </span>
-                        <?php endif; ?>
-                    </td>
-
-                    <td class="number">
-                        <?= $item['original_count'] ?>
-                    </td>
-
-                    <td class="number return">
-                    
-                        <span class="return">
                         
-                        <?php if ($item['count_remaining'] >  0 ): ?>
+
+                        <td class="number">
+                            <?= $item['original_count'] ?>
+                        </td>
+
+                        <td class="number return">
+                        
+                            <span class="return">
                             
-                                <a href="?uid=<?= $current_user_id ?>&log_loan=y&loan_id=<?= $item['loan_id'] ?>&diff=-1&code=<?= @$get_clear['code'] ?>">
-                                    <span class="button-minus">-</span>  
-                                </a>
-                            
-                        <?php endif;?>
-                        </span>        
-                        <?= $item['count_remaining'] ?>
-                        <span class="return">        
-                        <?php if ($item['count_remaining'] <  $item['original_count'] ): ?>
-                            
-                                <a href="?uid=<?= $current_user_id ?>&log_loan=y&loan_id=<?= $item['loan_id'] ?>&diff=1&code=<?= @$get_clear['code'] ?>">
-                                    <span class="button-plus">+</span>     
-                                </a>
-                            
-                        <?php endif;?>
-                        </span>
-                        <i class="icon loading"></i>
-                    </td>
-                    <td class="details" contenteditable="true"><?= $item['all_details'] ?></td>
-                </tr>                                
+                            <?php if ($item['count_remaining'] >  0 ): ?>
+                                
+                                    <a href="?uid=<?= $current_user_id ?>&log_loan=y&loan_id=<?= $item['loan_id'] ?>&diff=-1&code=<?= @$get_clear['code'] ?>">
+                                        <span class="button-minus">-</span>  
+                                    </a>
+                                
+                            <?php endif;?>
+                            </span>        
+                            <?= $item['count_remaining'] ?>
+                            <span class="return">        
+                            <?php if ($item['count_remaining'] <  $item['original_count'] ): ?>
+                                
+                                    <a href="?uid=<?= $current_user_id ?>&log_loan=y&loan_id=<?= $item['loan_id'] ?>&diff=1&code=<?= @$get_clear['code'] ?>">
+                                        <span class="button-plus">+</span>     
+                                    </a>
+                                
+                            <?php endif;?>
+                            </span>
+                            <i class="icon loading"></i>
+                        </td>
+                        <td class="details" contenteditable="true"><?= $item['all_details'] ?></td>
+                    </tr>                                
                 
                 <?php endforeach;?> 
             </tbody>
