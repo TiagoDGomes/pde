2a8ec6827651aad53a65ab2494bf8cc6ffd8bc4d
Tiago Gomes <dev@nerdbr.top>
2024-11-02 21:48:30 -0300

    basic template for patrimony details

--- include/show_patrimony.php
+++ include/show_patrimony.php
@@ -11,4 +11,39 @@ $selected_patrimony = Database::fetch($query, array($form_clear['pid']));
    <?php HTMLUtil::render_patrimony(NULL, $selected_patrimony['number1']) ; ?>
    <?= $selected_patrimony['name'] ?>
 </h2>
+
+<div class="details"> 
+   <dl>
+      <dt>Observações:</dt>
+      <dd><?= $selected_patrimony['obs'] ?></dd>
+         
+   </dl>
+</div>
+
+<div class="items">
+    <table>
+        <thead>
+            <tr>                
+                <th><input type="checkbox" name="chk_all"></th>
+                <th>Nome da pessoa</th>
+                <th>Cód. Ident. 1</th>
+                <th>Cód. Ident. 2</th>
+                <th>Data de devolução</th>
+                <th>Detalhes</th>
+            </tr>
+        </thead>
+        <tbody>
+         <tr>
+            <td></td>
+            <td></td>
+            <td class="number"></td>
+            <td class="number"></td>
+            <td class="number"></td>
+            <td></td>            
+         </tr>
+        </tbody>
+    </table>
+</div>
+
+
 <pre><?php var_dump($selected_patrimony); ?></pre>
