bffe7ece4ff8cd092eccbab9848dae9ee540262f
Tiago <tiago.gomes@ifsp.edu.br>
2024-11-06 16:38:25 -0300

    Corrigir posicionamento de div

--- include/show_user.php
+++ include/show_user.php
@@ -32,6 +32,8 @@
 <template id="tuser">
     <?php form_user($last_user_selected) ?>    
 </template>
+
+
 <div class="card user top">
     <h2>
         <i class="icon user"></i>
@@ -68,171 +70,168 @@
             }
         </script> 
     </div>
-<?php if (!$selected_user_loans) :?>
+    <?php if (!$selected_user_loans) :?>
 
-    <p>Nenhum empréstimo foi encontrado no período selecionado.</p>
+        <p>Nenhum empréstimo foi encontrado no período selecionado.</p>
 
-<?php else : ?>
-<?php include 'include/form_hidden_select.php'; ?>
+    <?php else : ?>
+        <?php include 'include/form_hidden_select.php'; ?>
 
     
-    <table>
-        <thead>
-            <tr>
-                <th colspan="2">
-                    <select onchange="select_action(this)">
-                        <option>Ações...</option>                     
-                        <option value="ret">&bullet; Devolver</option>    
-                        <option value="delete">&bullet; Excluir</option>                        
-                    </select>
-                </th>              
-                <th>Nome do item</th>
-                <th>Código</th>
-                <th>Quant. devolvida</th>
-                <th>Observações do<br>empréstimo</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php $last_date = NULL; ?>    
-            <?php foreach($selected_user_loans as $item): ?>    
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="2">
+                        <select onchange="select_action(this)">
+                            <option>Ações...</option>                     
+                            <option value="ret">&bullet; Devolver</option>    
+                            <option value="delete">&bullet; Excluir</option>                        
+                        </select>
+                    </th>              
+                    <th>Nome do item</th>
+                    <th>Código</th>
+                    <th>Quant. devolvida</th>
+                    <th>Observações do<br>empréstimo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $last_date = NULL; ?>    
+                <?php foreach($selected_user_loans as $item): ?>    
 
-                <?php $this_date = (new DateTimeImmutable($item['loan_date']))->format('d/m/Y'); ?>
+                    <?php $this_date = (new DateTimeImmutable($item['loan_date']))->format('d/m/Y'); ?>
 
-                <?php if ($last_date != $this_date): ?>
+                    <?php if ($last_date != $this_date): ?>
 
-                    <tr class="date">
-                        <th>
-                            <input onchange="select_all_date(this,'nid')" type="checkbox" class="loan_top_checkbox" id="loan_date_<?= str_replace("/","_", $this_date) ?>">
-                        </th>
-                        <th colspan="6">
-                            <label for="loan_date_<?= str_replace("/","_", $this_date) ?>" class="date"><?= $this_date ?></label> 
-                        </th>
-                    </tr>
+                        <tr class="date">
+                            <th>
+                                <input onchange="select_all_date(this,'nid')" type="checkbox" class="loan_top_checkbox" id="loan_date_<?= str_replace("/","_", $this_date) ?>">
+                            </th>
+                            <th colspan="6">
+                                <label for="loan_date_<?= str_replace("/","_", $this_date) ?>" class="date"><?= $this_date ?></label> 
+                            </th>
+                        </tr>
 
-                    <?php $last_date = $this_date; ?>
+                        <?php $last_date = $this_date; ?>
 
-                <?php endif; ?>                     
+                    <?php endif; ?>                     
 
-                <tr class="<?= $item['count_returned'] >= $item['original_count'] ? 'complete' : 'remaining' ?> loan_date_<?= str_replace("/","_",$last_date) ?>">
+                    <tr class="<?= $item['count_returned'] >= $item['original_count'] ? 'complete' : 'remaining' ?> loan_date_<?= str_replace("/","_",$last_date) ?>">
+                    
+                        <td>
+                            <input class="line_checkbox" data-id="<?= $item['loan_id'] ?>" onchange="select_item(this,'nid')" type="checkbox" id="loan_<?= $item['loan_id'] ?>">
+                        </td>
+
+                        <td class="number">
+
+                            <?php if ($item['patrimony_number']): ?>
+
+                                <?php HTMLUtil::render_patrimony($item['patrimony_id'], $item['patrimony_number'] ); ?> 
+                                <?= $item['patrimony_number2'] ? HTMLUtil::render_patrimony($item['patrimony_id'], $item['patrimony_number2'] ):''; ?> 
+
+                            <?php endif; ?>
+
+                        </td>
+
+                        <td>
+
+                            <label for="loan_<?= $item['loan_id'] ?>">
+
+                                <?= $item['model_name'] ?>
+
+                            </label>
+
+                        </td>
+
+                        <td class="number">
+
+                            <?= $item['model_code'] ?>
+
+                        </td>                   
+
+                        <td class="number return">
+                            <?php $input_hidden = array(); ?>
+                            <?php $input_hidden['iid'] = $item['model_id']; ?> 
+                            <?php $input_hidden['uid'] = $current_user_id; ?> 
+                            <?php $input_hidden['t'] = $current_query_type_string; ?> 
+                            <?php $input_hidden['q'] = $current_query_string; ?> 
+                            <?php $input_hidden['redirect_to'] = 'user'; ?> 
+                            <?php $input_hidden['act'] = 'ret'; ?> 
+                            <?php $input_hidden['nid'] = $item['loan_id']; ?>  
+
+                            <span class="return">
+                                
+                                <?php if ($item['count_returned'] >  0 ): ?>
+                                    
+                                    <?php $input_hidden['diff'] = '1'; ?> 
+                                    <a title="1 unidade a dever" href="?<?= http_build_query($input_hidden) ?>">
+                                        <span class="button-minus">-</span>  
+                                    </a>
+                                <?php else:?>
+                                    <!-- <span class="button-minus">&times;</span> -->
+                                <?php endif;?>
+
+                            </span>
+
+                            <span class="return">    
+                                
+                                <?php if ($item['count_returned'] <  $item['original_count'] ): ?>
+                                    
+                                    <?php $input_hidden['diff'] = '-1'; ?> 
+                                    <a title="1 unidade devolvida" href="?<?= http_build_query($input_hidden) ?>">
+                                        <span class="button-plus">+</span>     
+                                    </a>
+                                <?php else:?>
+                                    <!-- <span class="button-plus">&times;</span>     -->
+                                <?php endif;?>
+                                
+                            </span>
+
+                            <?= $item['count_returned'] . '/' . $item['original_count'] ?>
+
+                        </td>
+
+                        <td class="details">
+                            <ul class="loan_details" data-nid="<?= $item['loan_id'] ?>"
+                                id="loan_details_<?= $item['loan_id'] ?>"><?php 
+                                    $all_details = explode($all_details_separator_items, $item['all_details']);                                           
+                                    foreach($all_details as $detail):  
+                                        if ($detail){                                   
+                                            $detail_data = explode($all_details_separator_cols, $detail);  
+                                            if (strlen(trim($detail_data[2])) > 0) {
+                                                ?><li id="loan_detail_nnid_<?= $detail_data[0] ?>">
+                                                <span class="info" title="<?= $detail_data[1] ?>"><?= $detail_data[2] ?></span>                      
+                                                <span class="x" onclick="delete_loan_detail(<?= $detail_data[0] ?>)">&times;</span>
+                                            </li><?php }
+                                        }
+                                    endforeach;   
+                                ?></ul> 
+                            </ul>
+                            <div class="loan_details_new"
+                                    id="loan_details_new_<?= $item['loan_id'] ?>" 
+                                    onclick="show_loan_details_action(<?= $item['loan_id'] ?>)" 
+                                    onblur="hide_loan_details_action(<?= $item['loan_id'] ?>)" 
+                                    contenteditable="true">        
+                                
+                            </div>
+                            <div class="loan_details_action"
+                                id="loan_details_action_<?= $item['loan_id'] ?>" 
+                                style="display:none;text-align:center" >
+                                <a href="javascript:;" onclick="save_loan_details(<?= $item['loan_id'] ?>)">Registrar observações</a>
+                            </div>
+                            <script>
+                                // document.body.addEventListener('load', function(){
+                                //     var loan_details = document.querySelector('#loan_details_<?= $item['loan_id'] ?> span')
+                                //     loan_details.addEventListener('click', function(event){
+                                //         var nid = (event.target.parentNode.dataset['nid']);
+                                //     })
+                                // })
+                            </script>
+                        </td>
+                    </tr>                                
                 
-                    <td>
-                        <input class="line_checkbox" data-id="<?= $item['loan_id'] ?>" onchange="select_item(this,'nid')" type="checkbox" id="loan_<?= $item['loan_id'] ?>">
-                    </td>
+                <?php endforeach;?> 
 
-                    <td class="number">
-
-                        <?php if ($item['patrimony_number']): ?>
-
-                            <?php HTMLUtil::render_patrimony($item['patrimony_id'], $item['patrimony_number'] ); ?> 
-                            <?= $item['patrimony_number2'] ? HTMLUtil::render_patrimony($item['patrimony_id'], $item['patrimony_number2'] ):''; ?> 
-
-                        <?php endif; ?>
-
-                    </td>
-
-                    <td>
-
-                        <label for="loan_<?= $item['loan_id'] ?>">
-
-                            <?= $item['model_name'] ?>
-
-                        </label>
-
-                    </td>
-
-                    <td class="number">
-
-                        <?= $item['model_code'] ?>
-
-                    </td>                   
-
-                    <td class="number return">
-                        <?php $input_hidden = array(); ?>
-                        <?php $input_hidden['iid'] = $item['model_id']; ?> 
-                        <?php $input_hidden['uid'] = $current_user_id; ?> 
-                        <?php $input_hidden['t'] = $current_query_type_string; ?> 
-                        <?php $input_hidden['q'] = $current_query_string; ?> 
-                        <?php $input_hidden['redirect_to'] = 'user'; ?> 
-                        <?php $input_hidden['act'] = 'ret'; ?> 
-                        <?php $input_hidden['nid'] = $item['loan_id']; ?>  
-
-                        <span class="return">
-                            
-                            <?php if ($item['count_returned'] >  0 ): ?>
-                                
-                                <?php $input_hidden['diff'] = '1'; ?> 
-                                <a title="1 unidade a dever" href="?<?= http_build_query($input_hidden) ?>">
-                                    <span class="button-minus">-</span>  
-                                </a>
-                            <?php else:?>
-                                <!-- <span class="button-minus">&times;</span> -->
-                            <?php endif;?>
-
-                        </span>
-
-                        <span class="return">    
-                            
-                            <?php if ($item['count_returned'] <  $item['original_count'] ): ?>
-                                
-                                <?php $input_hidden['diff'] = '-1'; ?> 
-                                <a title="1 unidade devolvida" href="?<?= http_build_query($input_hidden) ?>">
-                                    <span class="button-plus">+</span>     
-                                </a>
-                            <?php else:?>
-                                <!-- <span class="button-plus">&times;</span>     -->
-                            <?php endif;?>
-                            
-                        </span>
-
-                        <?= $item['count_returned'] . '/' . $item['original_count'] ?>
-
-                    </td>
-
-                    <td class="details">
-                        <ul class="loan_details" data-nid="<?= $item['loan_id'] ?>"
-                            id="loan_details_<?= $item['loan_id'] ?>"><?php 
-                                $all_details = explode($all_details_separator_items, $item['all_details']);                                           
-                                foreach($all_details as $detail):  
-                                    if ($detail){                                   
-                                        $detail_data = explode($all_details_separator_cols, $detail);  
-                                        if (strlen(trim($detail_data[2])) > 0) {
-                                            ?><li id="loan_detail_nnid_<?= $detail_data[0] ?>">
-                                            <span class="info" title="<?= $detail_data[1] ?>"><?= $detail_data[2] ?></span>                      
-                                            <span class="x" onclick="delete_loan_detail(<?= $detail_data[0] ?>)">&times;</span>
-                                        </li><?php }
-                                    }
-                                endforeach;   
-                            ?></ul> 
-                        </ul>
-                        <div class="loan_details_new"
-                                id="loan_details_new_<?= $item['loan_id'] ?>" 
-                                onclick="show_loan_details_action(<?= $item['loan_id'] ?>)" 
-                                onblur="hide_loan_details_action(<?= $item['loan_id'] ?>)" 
-                                contenteditable="true">        
-                            
-                        </div>
-                        <div class="loan_details_action"
-                            id="loan_details_action_<?= $item['loan_id'] ?>" 
-                            style="display:none;text-align:center" >
-                            <a href="javascript:;" onclick="save_loan_details(<?= $item['loan_id'] ?>)">Registrar observações</a>
-                        </div>
-                        <script>
-                            // document.body.addEventListener('load', function(){
-                            //     var loan_details = document.querySelector('#loan_details_<?= $item['loan_id'] ?> span')
-                            //     loan_details.addEventListener('click', function(event){
-                            //         var nid = (event.target.parentNode.dataset['nid']);
-                            //     })
-                            // })
-                        </script>
-                    </td>
-                </tr>                                
-            
-            <?php endforeach;?> 
-
-        </tbody>
-    </table>
+            </tbody>
+        </table>   
+    <?php endif; ?> 
 </div>
-<?php endif; ?>
-<script>
-
-</script>
