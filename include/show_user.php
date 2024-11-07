6faf1b875946f246aa424ff72c5bad021aa34cc7
Tiago <tiago.gomes@ifsp.edu.br>
2024-11-07 20:05:01 -0300

    Usando date_filter para usuário

--- include/show_user.php
+++ include/show_user.php
@@ -44,32 +44,10 @@
 </div>
 
 
-<div class="central card items">
-    <div class="filter">
-        <form>    
-            <input type="hidden" name="uid" value="<?= $form_clear['uid'] ?>">   
-            <input type="hidden" name="q" value="<?= @$form_clear['q'] ?>">     
-            <input type="hidden" name="t" value="<?= @$form_clear['t'] ?>">    
-            <label>Entre:
-                <input id="chk_date_before" onchange="date_change(this)" type="date" name="before" max="<?= $current_date_now ?>" value="<?=$current_date_before ?>">
-            </label>        
-            <label> e
-                <input id="chk_date_after" onchange="date_change(this)" type="date" name="after" max="<?= $current_date_now ?>" value="<?= $current_date_after ?>">
-            </label>
-            <button>Filtrar</button>  
-        </form>     
-        <script>
-            function date_change(elem){
-                var chk_date_before = document.getElementById('chk_date_before');
-                var chk_date_after = document.getElementById('chk_date_after');
-                if (elem == chk_date_before){
-                    chk_date_after.min = chk_date_before.value;
-                } else {
-                    chk_date_before.max = chk_date_after.value;
-                }
-            }
-        </script> 
-    </div>
+<div class="central card items table-items">
+    
+    <?php include 'include/date_filter.php'; ?>
+
     <?php if (!$selected_user_loans) :?>
 
         <p>Nenhum empréstimo foi encontrado no período selecionado.</p>
