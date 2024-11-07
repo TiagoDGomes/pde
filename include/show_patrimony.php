9627a73e3cf725870703a9a488bcc4e6b3e6f273
Tiago Gomes <dev@nerdbr.top>
2024-11-06 23:10:32 -0300

    Empréstimos de patrimônio

--- include/show_patrimony.php
+++ include/show_patrimony.php
@@ -6,6 +6,34 @@ $query = "SELECT * FROM patrimony p
             WHERE p.id = ?";
 $selected_patrimony = Database::fetch($query, array($form_clear['pid']));        
 
+
+$query_search_loans = "SELECT 
+                     u.name AS username,
+                     p.id as patrimony_id, 
+                     n.tstamp as loan_date, 
+                     p.number1 as patrimony_number, 
+                     p.number2 as patrimony_number2, 
+                     p.serial_number as patrimony_serial_number, 
+                     original_count,
+                     CASE 
+                        WHEN original_count - sum(diff) > original_count THEN original_count
+                        ELSE original_count - sum(diff) END as count_returned, 
+                     $all_details_sql_concat as all_details,
+                     n.id as loan_id
+                FROM loan n
+                INNER JOIN log_loan nn ON (nn.loan_id = n.id)
+                INNER JOIN user u ON (u.id = n.user_id)
+                LEFT JOIN patrimony p ON (n.patrimony_id = p.id)
+                WHERE p.id = ?
+                GROUP BY n.id
+                ORDER BY n.tstamp DESC
+        ";
+        $current_date_after_1day =  (new DateTimeImmutable($current_date_after . ' +1 day'))->format('Y-m-d');     
+
+        $params = array($form_clear['pid']);
+        $selected_loans = Database::fetchAll($query_search_loans, $params);
+
+
 ?>
 
 <div class="card item top">
@@ -23,29 +51,36 @@ $selected_patrimony = Database::fetch($query, array($form_clear['pid']));
     <p class="bar"><button>Editar</button></p>
 </div>
 
-
+<?php if (count($selected_loans)==0): ?>
+    <p>Nenhum empréstimo foi encontrado.</p>
+<?php else: ?>  
 <div class="card items">
+    <h3>Empréstimos deste patrimônio</h3>
     <table>
         <thead>
             <tr>                
                 <th><input disabled type="checkbox" name="chk_all"></th>
                 <th>Nome da pessoa</th>
-                <th>Cód. Ident. 1</th>
-                <th>Cód. Ident. 2</th>
+                <th></th>
+                <th></th>
                 <th>Data de devolução</th>
                 <th>Detalhes</th>
             </tr>
         </thead>
         <tbody>
-         <tr>
-            <td></td>
-            <td></td>
-            <td class="number"></td>
-            <td class="number"></td>
-            <td class="number"></td>
-            <td></td>            
-         </tr>
+            <?php foreach($selected_loans as $item): ?> 
+
+                <tr>
+                    <td><input disabled type="checkbox" name="chk_all"></td>
+                    <td><?= $item['username'] ?></td>
+                    <td class="number"></td>
+                    <td class="number"></td>
+                    <td class="number"><?= $item['loan_date'] ?></td>
+                    <td></td>            
+                </tr>
+            <?php endforeach; ?>
         </tbody>
     </table>
 </div>
 
+<?php endif; ?>
