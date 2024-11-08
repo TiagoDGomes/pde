de91f5d292c283746de1df4cd4217c1b20141a74
Tiago Gomes <dev@nerdbr.top>
2024-11-07 23:05:00 -0300

    uid form_hidden_select não está dinponível na página inicial

--- include/form_hidden_select.php
+++ include/form_hidden_select.php
@@ -1,6 +1,6 @@
 <form id="form_hidden_select" method="">
     <input type="hidden" name="multiact" value="">
-    <input type="hidden" name="uid" value="<?= $form_clear['uid'] ?>">   
+    <?php if (isset($form_clear['uid'])): ?><input type="hidden" name="uid" value="<?= $form_clear['uid'] ?>"><?php endif; ?>  
     <input type="hidden" name="q" value="<?= @$form_clear['q'] ?>">     
     <input type="hidden" name="t" value="<?= @$form_clear['t'] ?>">  
 </form>
