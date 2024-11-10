fac4718cb8f816102e4235133413d9a0fff6b482
Tiago Gomes <dev@nerdbr.top>
2024-11-10 12:13:24 -0300

    Listando empréstimos de não patrimoniados

--- include/queries/item.php
+++ include/queries/item.php
@@ -3,50 +3,82 @@
 $query = "SELECT * FROM model m
             WHERE m.id = ?";
 $selected_item = Database::fetch($query, array($form_clear['iid']));        
+if ($selected_item['has_patrimony']){
+   $query_search_loans = "SELECT m.id as model_id, 
+         m.name as model_name,
+         m.code as model_code, 
+         p.id as patrimony_id, 
+         max(n.tstamp) as loan_date, 
+         p.number1 as patrimony_number, 
+         p.number2 as patrimony_number2, 
+         p.obs as obs,
+         p.patrimony_location as patrimony_location,
+         m.model_location as model_location,
+         u.id as uid,
+         u.name as username,
+         u.code1 as code1,
+         u.code2 as code2,
+         p.loan_block as loan_block,
+         CASE WHEN 
+            p.loan_block = 1 or p.usable = 0  or p.found = 0 THEN 'blocked'
+            ELSE 'ok' END AS icon_block,
+         CASE WHEN 
+            p.usable = 1  THEN 'usable'
+            ELSE 'trash' END AS icon_usable,
+         CASE WHEN 
+            p.found = 1  THEN 'found'
+            ELSE 'unknown' END AS icon_found,
+         p.serial_number as patrimony_serial_number, 
+         original_count,
+         CASE 
+            WHEN original_count - sum(diff) > original_count THEN original_count
+            ELSE original_count - sum(diff) END as count_returned, 
+         group_concat(details, '<br>') as all_details ,
+         n.id as loan_id
+      FROM patrimony p 
+      INNER JOIN model m ON (p.model_id = m.id)
+      LEFT JOIN loan n ON (n.model_id = m.id and p.id = n.patrimony_id)
+      LEFT JOIN user u ON (u.id = n.user_id)
+      LEFT JOIN log_loan nn ON (nn.loan_id = n.id)
+      WHERE p.model_id =  ? 
+      GROUP BY p.id
+      ORDER BY n.id DESC
+      LIMIT 300
+      ";
 
-$query_search_loans = "SELECT m.id as model_id, 
-                     m.name as model_name,
-                     m.code as model_code, 
-                     p.id as patrimony_id, 
-                     max(n.tstamp) as loan_date, 
-                     p.number1 as patrimony_number, 
-                     p.number2 as patrimony_number2, 
-                     p.obs as obs,
-                     p.patrimony_location as patrimony_location,
-                     m.model_location as model_location,
-                     u.id as uid,
-                     u.name as username,
-                     u.code1 as code1,
-                     u.code2 as code2,
-                     p.loan_block as loan_block,
-                     CASE WHEN 
-                        p.loan_block = 1 or p.usable = 0  or p.found = 0 THEN 'blocked'
-                        ELSE 'ok' END AS icon_block,
-                     CASE WHEN 
-                        p.usable = 1  THEN 'usable'
-                        ELSE 'trash' END AS icon_usable,
-                     CASE WHEN 
-                        p.found = 1  THEN 'found'
-                        ELSE 'unknown' END AS icon_found,
-                     p.serial_number as patrimony_serial_number, 
-                     original_count,
-                     CASE 
-                        WHEN original_count - sum(diff) > original_count THEN original_count
-                        ELSE original_count - sum(diff) END as count_returned, 
-                     group_concat(details, '<br>') as all_details ,
-                     n.id as loan_id
-                FROM patrimony p 
-                INNER JOIN model m ON (p.model_id = m.id)
-                LEFT JOIN loan n ON (n.model_id = m.id and p.id = n.patrimony_id)
-				LEFT JOIN user u ON (u.id = n.user_id)
-                LEFT JOIN log_loan nn ON (nn.loan_id = n.id)
-                WHERE p.model_id =  ? 
-                -- AND n.tstamp BETWEEN ? AND ?
-                GROUP BY p.id
-			   -- HAVING n.id = max(n.id) OR n.id IS NULL
-                ORDER BY n.id DESC
-        ";
-        $current_date_after_1day =  (new DateTimeImmutable($current_date_after . ' +1 day'))->format('Y-m-d');     
 
-        $params = array($form_clear['iid']);
-        $selected_loans = Database::fetchAll($query_search_loans, $params);
+
+} else {
+   $query_search_loans = "SELECT m.id as model_id, 
+      m.name as model_name,
+      m.code as model_code, 
+      max(n.tstamp) as loan_date, 
+      m.model_location as model_location,
+      u.id as uid,
+      u.name as username,
+      u.code1 as code1,
+      u.code2 as code2,
+      original_count,         
+      CASE WHEN 
+            m.model_loan_block = 1 THEN 'blocked'
+            ELSE 'ok' END AS icon_block,
+      CASE 
+         WHEN original_count - sum(diff) > original_count THEN original_count
+         ELSE original_count - sum(diff) END as count_returned, 
+      group_concat(details, '<br>') as all_details ,
+      n.id as loan_id
+   FROM  model m 
+   LEFT JOIN loan n ON (n.model_id = m.id)
+   LEFT JOIN user u ON (u.id = n.user_id)
+   LEFT JOIN log_loan nn ON (nn.loan_id = n.id)
+   WHERE m.id =  ? 
+   GROUP BY m.id
+   ORDER BY n.id DESC
+   LIMIT 300
+   ";
+}
+
+$current_date_after_1day =  (new DateTimeImmutable($current_date_after . ' +1 day'))->format('Y-m-d');     
+
+$params = array($form_clear['iid']);
+$selected_loans = Database::fetchAll($query_search_loans, $params);
---
+++ include/show_item_not_patrimony.php
@@ -0,0 +1,58 @@
+<?php isset($PDE) or die('Nope');
+require_once 'include/form_patrimony.php'; ?>
+<template id="tpatrimony">
+
+    <?php form_patrimony(array_merge($selected_item, 
+                                        array("patrimony_location"=>'',
+                                                "obs" => '', 
+                                                'model_id' => $selected_item['id'],
+                                                'id' => NULL,
+                        ))); ?>
+</template>
+<?php if (count($selected_loans)==0): ?>
+    <p>Nenhum empréstimo foi encontrado.</p>
+<?php else: ?>    
+    <div class=" items">
+        <table>
+            <thead>
+                <tr>                
+                    <th><input disabled type="checkbox" name="chk_all"></th>
+                    <th>Data do último<br> empréstimo</th>
+                    <th>Nome da pessoa</th>
+                    <th>Detalhes</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+            <?php foreach($selected_loans as $item): ?> 
+
+            <tr class="<?= $item['icon_block'] ?>">
+                <td><input disabled type="checkbox" name="chk_all"></td>
+                <td class="date">
+                    <i class="icon <?= $item['original_count'] <= $item['count_returned'] ? 'check' : 'cart' ?>"><span></span></i>
+                    <?= $item['loan_date'] ? (new DateTimeImmutable( $item['loan_date'] ))->format('d/m/Y H:i:s') : ''?>
+                    
+                    
+                </td>
+                <td>
+                    <a href="?uid=<?= $item['uid'] ?>">
+                        <?= $item['username'] ?>
+                    </a>
+                </td>  
+                <td>
+                    <?php if ($item['original_count'] > $item['count_returned'] ) : ?>
+                        <?php $input_hidden['act'] = 'ret'; ?> 
+                        <?php $input_hidden['redirect_to'] = 'item'; ?> 
+                        <?php $input_hidden['diff'] = '-1'; ?> 
+                        <?php $input_hidden['nid'] = $item['loan_id']; ?> 
+                        <?php $input_hidden['iid'] = $item['model_id']; ?>                     
+                        <i class="icon check"></i>
+                        <a href="?<?= http_build_query($input_hidden) ?>">Marcar como devolvido</a>
+                    <?php endif; ?>    
+                </td>            
+            </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+<?php endif; ?>
